14d75997d82a41792df8fcdd699c1f8f91f249fa
Zachary Fong <fong.zachary@gmail.com>
2016-08-25 08:49:40 -0700

    add geoquakes

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 
-// $(document).ready(function(){
-//   $('span').on('click', function rate(){
+$(document).ready(function(){
+  $('span').on('click', function rate(){
+     $(this).css("background-color","#1E633C");
+  })
+})
 
-//   })
-// })
-
-var rating = document.getElementsByTagName('span');
-rating.addEventListener('click', rate);
-
-var rate = function(event){
-  document.getElementsByTagName("span").style.background = "#1E633C";
-}
+// var rateMe = document.getElementsByTagName('span');
+// rateMe.addEventListener('click', function rate(event){
+//   document.getElementsByTagName("span").style.background = "#1E633C";
+//});
